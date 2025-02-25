2c56cf12c1d2f5544e2b9f447546a9a824815c64
hdemerald <tripled5.hunter5@gmail.com>
2025-02-24 21:45:05 -0800

    changed sizes to match paper's dimensions

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 import network
-import mnist_loader as loader
+import loader
 
 # Load in MNIST Data for training
 (train_data, valid_data, test_data) = loader.load_data_wrapper()
 
 # List of sizes of each layer of nodes
-sizes = [784, 16, 16, 10]
+sizes = [19*19, 
+         19*19*12, 
+         21*21*8, 
+         21*21*80, 
+         13*13*33, 
+         13*13*97, 
+         7*7*64, 
+         3*3*47, 
+         1*35]
 
 # Create neural network
 nn = network.Network(sizes)
@@ -14,4 +22,6 @@ nn = network.Network(sizes)
 nn.SGD(train_data, 25, 10, 0.05)
 
 # Evaluate the model and print the correct results
-print(str(nn.evaluate(test_data)) + " correct results out of " + str(len(test_data)))
+print(str(nn.evaluate(test_data)) + 
+      " correct results out of " + 
+      str(len(test_data)))
